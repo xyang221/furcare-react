de1608febde4ca423f4f84d822e9d5ee184f9e3f
Dianne Bastillada <diannebastillada87@gmail.com>
2023-12-25 11:29:25 +0800

    generate billing process completed

--- src/pages/Billing/GenerateBilling.jsx
+++ src/pages/Billing/GenerateBilling.jsx
@@ -1,29 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axiosClient from "../../axios-client";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import {
-  Alert,
   Box,
   Button,
   IconButton,
   Paper,
-  Stack,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
-  TablePagination,
   TableRow,
   TextField,
   Typography,
 } from "@mui/material";
-import { Add, Close, Refresh, Remove } from "@mui/icons-material";
-import { useRef } from "react";
+import { Refresh } from "@mui/icons-material";
+import Swal from "sweetalert2";
 
 export default function GenerateBilling() {
   const { id } = useParams();
-  const navigate = useNavigate();
+
   //for table
   const columns = [
     { id: "Pet", name: "Pet" },
@@ -35,24 +32,28 @@ export default function GenerateBilling() {
 
   const [message, setMessage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [date, setDate] = useState(new Date());
 
   const [servicesavailed, setServicesavailed] = useState([]);
   const [clientservice, setClientservice] = useState([]);
-  const [servicesavailedpaid, setServicesavailedpaid] = useState([]);
   const [petowner, setPetowner] = useState([]);
 
   const [serviceAvailedPrices, setServiceAvailedPrices] = useState({});
   const [cash, setCash] = useState(null);
 
   const getServicesAvailed = () => {
+    setCash(null);
     setMessage(null);
     setLoading(true);
+    setServicesavailed([]);
+    setServiceAvailedPrices({});
     axiosClient
       .get(`/servicesavailed/petowner/${id}`)
       .then(({ data }) => {
         setLoading(false);
         setServicesavailed(data.data);
+        data.data.forEach((item) =>
+          updateServicePrice(item.id, item.unit_price)
+        );
       })
       .catch((mes) => {
         const response = mes.response;
@@ -79,24 +80,6 @@ export default function GenerateBilling() {
       });
   };
 
-  const getServicesAvailedChargeSlip = () => {
-    setMessage(null);
-    setLoading(true);
-    axiosClient
-      .get(`/servicesavailed/petowner/${id}/completed`)
-      .then(({ data }) => {
-        setLoading(false);
-        setServicesavailedpaid(data.data);
-      })
-      .catch((mes) => {
-        const response = mes.response;
-        if (response && response.status == 404) {
-          setMessage(response.data.message);
-        }
-        setLoading(false);
-      });
-  };
-
   const updateServicePrice = (id, price) => {
     setServiceAvailedPrices((prevPrices) => ({
       ...prevPrices,
@@ -113,7 +96,7 @@ export default function GenerateBilling() {
   // Calculate the total for all selected items
   const calculateTotal = () => {
     return servicesavailed.reduce((total, item) => {
-      const price = serviceAvailedPrices[item.id] || 0; // Fetch price from state
+      const price = serviceAvailedPrices[item.id] || 0;
       return total + price * item.quantity;
     }, 0);
   };
@@ -124,17 +107,12 @@ export default function GenerateBilling() {
     if (balance === undefined || isNaN(balance)) {
       return 0;
     }
-    // const newBalance = calculateBalance();
-    // setClientservice(prevState => ({
-    //   ...prevState,
-    //   balance: newBalance
-    // }));
+    if (cash > balance) {
+      return 0;
+    }
     return Math.max(balance, 0);
   };
 
-  // const balance = calculateBalance()
-
-  // Update the onChange function for the price TextField
   const handleCash = (ev, id) => {
     const { value } = ev.target;
     setCash(parseFloat(value));
@@ -153,27 +131,14 @@ export default function GenerateBilling() {
       return 0;
     }
 
-    const change = cash - calculateBalance();
+    const change =
+      cash - (calculateTotal() + clientservice.deposit - clientservice.balance);
     return Math.max(change, 0); // Ensure the result stays at a minimum of 0
   };
 
-  // // Group services by pet
-  // const groupServicesByPet = () => {
-  //   const groupedServices = {};
-  //   servicesavailed.forEach((item) => {
-  //     const petId = item.pet.id;
-  //     if (groupedServices.hasOwnProperty(petId)) {
-  //       groupedServices[petId].push(item);
-  //     } else {
-  //       groupedServices[petId] = [item];
-  //     }
-  //   });
-  //   return groupedServices;
-  // };
+  const onSubmit = async (ev) => {
+    ev.preventDefault();
 
-  // const servicesGroupedByPet = groupServicesByPet();
-
-  const onSubmit = async () => {
     try {
       const updatedServicesPromises = servicesavailed.map((item) => {
         return axiosClient.put(`/servicesavailed/${item.id}`, {
@@ -182,23 +147,39 @@ export default function GenerateBilling() {
         });
       });
 
-      const updateServicesResults = await Promise.all(updatedServicesPromises);
+      await Promise.all(updatedServicesPromises);
 
-      await axiosClient.put(`/clientservices/${clientservice.id}`, {
-        ...clientservice,
-        balance: calculateBalance() || null,
-      });
+      if (clientservice) {
+        // Calculate and update client's balance
+        const updatedClientService = {
+          ...clientservice,
+          balance: calculateBalance() || 0,
+        };
 
-      getServicesAvailed(); // Assuming this is a function defined elsewhere
-      navigate(`/admin/${id}/chargeslip`);
-      // servicesGroupedByPet(null);
-      getServicesAvailedChargeSlip(); // Assuming this is a function defined elsewhere
-    } catch (err) {
-      if (err.response && err.response.status === 422) {
-        console.log(err.response.data.errors);
-      } else {
-        console.log(err.response.data.message);
+        await axiosClient.put(
+          `/clientservices/${clientservice.id}`,
+          updatedClientService
+        );
+        Swal.fire({
+          title: "Success",
+          icon: "success",
+          confirmButtonText: "PRINT CHARGE SLIP",
+          confirmButtonColor: "black",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            setCash(0);
+            getServicesAvailed();
+            windowOpenPDFforPrint();
+          }
+        });
       }
+    } catch (err) {
+      Swal.fire({
+        title: "Error",
+        text: "No services availed!",
+        icon: "error",
+        confirmButtonColor: "black",
+      });
     }
   };
 
@@ -207,14 +188,47 @@ export default function GenerateBilling() {
     getClientService();
   };
 
-  useEffect(() => {}, []);
+  const windowOpenPDFforPrint = async () => {
+    try {
+      // Fetch PDF content
+      const response = await axiosClient.get(
+        `/clientservice/${clientservice.id}/generate-chargeslip`,
+        {
+          responseType: "blob",
+          headers: {
+            "Content-Type": "application/pdf",
+          },
+        }
+      );
+
+      const pdfBlob = response.data;
+
+      const url = window.URL.createObjectURL(new Blob([pdfBlob]));
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute(
+        "download",
+        `ChargeSlip-${
+          clientservice.date
+        }-${`${petowner.firstname}_${petowner.lastname}`}-.pdf`
+      );
+      document.body.appendChild(link);
+
+      // Trigger the download
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      alert("Error fetching PDF:", error);
+    }
+  };
 
   return (
     <>
       <Paper
         sx={{
-          width: "550px",
-          padding: "10px",
+          width: "650px",
+          padding: "5px",
           margin: "auto",
         }}
       >
@@ -225,7 +239,7 @@ export default function GenerateBilling() {
           </IconButton>
         </Typography>{" "}
         <Box
-          p={2}
+          p={1}
           display="flex"
           flexDirection="row"
           justifyContent="space-between"
@@ -233,119 +247,112 @@ export default function GenerateBilling() {
           <Typography variant="subtitle">
             Client: {petowner.firstname} {petowner.lastname}
           </Typography>
-          <Typography variant="subtitle">
-            Date: {date.toDateString()}{" "}
-          </Typography>
+          <Typography variant="subtitle">Date: {clientservice.date}</Typography>
         </Box>
         <TableContainer sx={{ height: 380 }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell key={column.id}>{column.name}</TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            {loading && (
-              <TableBody>
+          <form onSubmit={(ev) => onSubmit(ev)}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
                 <TableRow>
-                  <TableCell colSpan={6} style={{ textAlign: "center" }}>
-                    Loading...
-                  </TableCell>
+                  {columns.map((column) => (
+                    <TableCell key={column.id}>{column.name}</TableCell>
+                  ))}
                 </TableRow>
-              </TableBody>
-            )}
-            {!loading && message && (
-              <TableBody>
-                <TableRow>
-                  <TableCell colSpan={6} style={{ textAlign: "center" }}>
-                    {message}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            )}
-            {!loading && (
-              <TableBody>
-                {/* {Object.keys(servicesGroupedByPet).map((petId) => (
-                  <React.Fragment key={petId}>
-                    <TableRow>
-                      <TableCell colSpan={5}>
-                        <Typography variant="subtitle1">
-                          {servicesGroupedByPet[petId][0].pet.name}
-                        </Typography>
+              </TableHead>
+              {loading && (
+                <TableBody>
+                  <TableRow>
+                    <TableCell colSpan={6} style={{ textAlign: "center" }}>
+                      Loading...
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              )}
+              {!loading && message && (
+                <TableBody>
+                  <TableRow>
+                    <TableCell
+                      colSpan={6}
+                      style={{ textAlign: "center", color: "red" }}
+                    >
+                      {message}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              )}
+              {!loading && (
+                <TableBody>
+                  {servicesavailed.map((item) => (
+                    <TableRow hover role="checkbox" key={item.id}>
+                      <TableCell>{item.pet.name} </TableCell>
+                      <TableCell>{item.service.service}</TableCell>
+                      <TableCell>{item.quantity}</TableCell>
+                      <TableCell>{item.unit}</TableCell>
+                      <TableCell sx={{ width: "30%" }}>
+                        <TextField
+                          key={item.id}
+                          size="small"
+                          type="number"
+                          value={serviceAvailedPrices[item.id] || ""}
+                          onChange={(ev) => handlePriceChange(ev, item.id)}
+                          required
+                        />
                       </TableCell>
-                    </TableRow> */}
-                {servicesavailed.map((item) => (
-                  <TableRow hover role="checkbox" key={item.id}>
-                    <TableCell>{item.pet.name} </TableCell>
-                    <TableCell>{item.service.service}</TableCell>
-                    <TableCell>{item.quantity}</TableCell>
-                    <TableCell>{item.unit}</TableCell>
-                    <TableCell sx={{ width: "50%" }}>
+                    </TableRow>
+                  ))}
+                  <TableRow>
+                    <TableCell colSpan={4} align="right">
+                      Total:
+                    </TableCell>
+                    <TableCell>{calculateTotal()}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={4} align="right">
+                      Deposit:
+                    </TableCell>
+                    <TableCell>
+                      {calculateTotal() === 0 ? 0 : clientservice.deposit}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={4} align="right">
+                      Balance:
+                    </TableCell>
+                    <TableCell sx={{ width: "30%" }}>
+                      {calculateBalance()}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell colSpan={4} align="right">
+                      Cash:
+                    </TableCell>
+                    <TableCell sx={{ width: "30%" }}>
                       <TextField
                         size="small"
                         type="number"
-                        // value={
-                        //   item.unit_price === null
-                        //     ? serviceAvailedPrices[item.id]
-                        //     : item.unit_price
-                        // }
-                        value={serviceAvailedPrices[item.id]}
-                        onChange={(ev) => handlePriceChange(ev, item.id)}
-                        required
+                        onChange={(ev) => handleCash(ev)}
+                        value={cash || ""}
                       />
                     </TableCell>
                   </TableRow>
-                ))}
-                {/* </React.Fragment> */}
-                {/* ))} */}
-                <TableRow>
-                  <TableCell colSpan={4} align="right">
-                    Total:
-                  </TableCell>
-                  <TableCell>{calculateTotal()}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell colSpan={4} align="right">
-                    Deposit:
-                  </TableCell>
-                  <TableCell>
-                    {calculateTotal() === 0 ? 0 : clientservice.deposit}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell colSpan={4} align="right">
-                    Balance:
-                  </TableCell>
-                  <TableCell sx={{ width: "30%" }}>
-                    {calculateBalance()}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell colSpan={4} align="right">
-                    Cash:
-                  </TableCell>
-                  <TableCell sx={{ width: "30%" }}>
-                    <TextField
-                      size="small"
-                      type="number"
-                      onChange={(ev) => handleCash(ev)}
-                      value={cash || ""}
-                    />
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell colSpan={4} align="right">
-                    Change:
-                  </TableCell>
-                  <TableCell>{calculateChange()}</TableCell>
-                </TableRow>
-              </TableBody>
-            )}
-            <Button variant="contained" size="small" onClick={onSubmit}>
-              <Typography>Generate</Typography>
+                  <TableRow>
+                    <TableCell colSpan={4} align="right">
+                      Change:
+                    </TableCell>
+                    <TableCell>{calculateChange()}</TableCell>
+                  </TableRow>
+                </TableBody>
+              )}
+            </Table>
+            <Button
+              variant="contained"
+              type="submit"
+              fullWidth
+              sx={{ marginTop: "10px" }}
+            >
+              Pay
             </Button>
-          </Table>
+          </form>
         </TableContainer>
       </Paper>
     </>

f73686a527cbf3b7c33d2e69d80f9ddd2023b6d8
Dianne Bastillada <diannebastillada87@gmail.com>
2023-12-28 10:47:37 +0800

    treatment form completed

--- src/pages/TreatmentForm.jsx
+++ src/pages/TreatmentForm.jsx
@@ -1,30 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import axiosClient from "../axios-client";
-import { useStateContext } from "../contexts/ContextProvider";
 import {
   Box,
   Button,
-  FormControl,
-  IconButton,
-  InputLabel,
-  MenuItem,
+  Divider,
   Paper,
-  Select,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
 import PetConditionAdmission from "./PetConditionAdmission";
 import PetMedicationAdmission from "./PetMedicationAdmission";
-import { Refresh } from "@mui/icons-material";
 
 export default function TreatmentForm({ sid }) {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState(null);
-  const { setNotification } = useStateContext();
 
   const [treatment, setTreatment] = useState({
     id: null,
@@ -42,21 +34,17 @@ export default function TreatmentForm({ sid }) {
     comments: "",
   });
 
-  const [pets, setPets] = useState([]);
-  const getPetownerPets = () => {
-    axiosClient
-      .get(`/petowners/${id}/pets`)
-      .then(({ data }) => {
-        setPets(data.data);
-      })
-      .catch(() => {});
-  };
+  const [pet, setPet] = useState([]);
+  const [breed, setBreed] = useState([]);
+  const [edittreatment, setEdittreatment] = useState(false);
 
   const getCurrentTreatment = () => {
     axiosClient
-      .get(`/treatments/petowner/${id}/service/${sid}`)
+      .get(`/treatments/${id}`)
       .then(({ data }) => {
         setTreatment(data);
+        setPet(data.pet);
+        setBreed(data.pet.breed);
       })
       .catch(() => {});
   };
@@ -64,12 +52,11 @@ export default function TreatmentForm({ sid }) {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (treatment.id) {
+    if (edittreatment) {
       axiosClient
         .put(`/treatments/${treatment.id}`, treatment)
         .then(() => {
-          setNotification("treatment successfully updated");
-          navigate("/treatments");
+          setEdittreatment(false);
         })
         .catch((err) => {
           const response = err.response;
@@ -82,8 +69,8 @@ export default function TreatmentForm({ sid }) {
         .post(`/treatments/petowner/${id}/service/${sid}`, treatment)
         .then((response) => {
           console.log(treatment);
-          setNotification("treatment successfully created");
           setTreatment(response.data);
+          setEdittreatment(false);
         })
         .catch((err) => {
           const response = err.response;
@@ -102,211 +89,273 @@ export default function TreatmentForm({ sid }) {
     setTreatment(updatedTreatment);
   };
 
-  const handleRefresh = () => {
-    getPetownerPets();
-    getCurrentTreatment();
+  const onEdit = () => {
+    setEdittreatment(true);
   };
+
+  useEffect(() => {
+    getCurrentTreatment();
+  }, []);
 
   return (
     <Paper
       sx={{
-        display: "flex",
+        width: "80%",
+        margin: "auto",
       }}
     >
       <Stack
         sx={{
-          border: "1px solid black",
-          margin: "10px",
-          padding: "10px",
+          margin: "5px",
+          padding: "5px",
           display: "flex",
           textAlign: "center",
         }}
       >
-        <Typography variant="h4">
-          Treatment Sheet{" "}
-          <IconButton color="success" onClick={handleRefresh}>
-            <Refresh />
-          </IconButton>
-        </Typography>
-        {loading && <div className="text-center">Loading...</div>}
-        {errors && (
-          <div className="alert">
-            {Object.keys(errors).map((key) => (
-              <p key={key}>{errors[key][0]}</p>
-            ))}
-          </div>
-        )}
-        {!loading && (
-          <form onSubmit={(e) => onSubmit(e)}>
-            {treatment.id ? (
-              <Typography variant="h6">Date: {treatment.date}</Typography>
-            ) : (
-              <Typography variant="h6">Date: {date.toDateString()} </Typography>
-            )}
-            <Typography variant="h6">Day: ? </Typography>
-            <Box>
-              <TextField
-                value={treatment.diagnosis}
-                onChange={(ev) =>
-                  handleFieldChange("diagnosis", ev.target.value)
-                }
-                label="Diagnosis/Findings"
-                variant="outlined"
-                size="small"
-              rows={2}
-                fullWidth
-                multiline
-                required
-              />
-            </Box>
-            <br></br>
-            <FormControl fullWidth>
-              <InputLabel>Pet</InputLabel>
-              <Select
-                label="Pet"
-                value={treatment.pet_id || ""}
-                onChange={(ev) => handleFieldChange("pet_id", ev.target.value)}
-                fullWidth
-                variant="standard"
-              >
-                {pets.map((item) => (
-                  <MenuItem key={item.id} value={item.id}>
-                    {/* {item.name} */}
-                    {`${item.name} (Breed: ${item.breed.breed})`}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <br></br>
-            <Stack flexDirection={"row"}>
-              <Stack display={"flex"} flexDirection={"column"} padding={"10px"}>
-                <TextField
-                  value={treatment.body_weight}
-                  onChange={(ev) =>
-                    handleFieldChange("body_weight", ev.target.value)
-                  }
-                  label="BW"
-                  variant="standard"
-                  size="small"
-                  required
-                  type="number"
-                />
-                <TextField
-                  value={treatment.heart_rate}
-                  onChange={(ev) =>
-                    handleFieldChange("heart_rate", ev.target.value)
-                  }
-                  label="HR"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
-                <TextField
-                  value={treatment.mucous_membranes}
-                  onChange={(ev) =>
-                    handleFieldChange("mucous_membranes", ev.target.value)
-                  }
-                  label="MM"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
-                <TextField
-                  value={treatment.pr_prealbumin}
-                  onChange={(ev) =>
-                    handleFieldChange("pr_prealbumin", ev.target.value)
-                  }
-                  label="PR"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
-                <TextField
-                  value={treatment.temperature}
-                  onChange={(ev) =>
-                    handleFieldChange("temperature", ev.target.value)
-                  }
-                  label="Temp"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
-              </Stack>
-              <Stack display={"flex"} flexDirection={"column"} padding={"10px"}>
-                <TextField
-                  value={treatment.respiration_rate}
-                  onChange={(ev) =>
-                    handleFieldChange("respiration_rate", ev.target.value)
-                  }
-                  label="RR"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
+        <Button
+          variant="contained"
+          onClick={() => navigate(-1)}
+          sx={{ width: "10%" }}
+        >
+          back
+        </Button>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <Typography variant="h5" fontWeight={"bold"}>
+            Treatment Sheet{" "}
+          </Typography>
 
-                <TextField
-                  value={treatment.caspillar_refill_time}
-                  onChange={(ev) =>
-                    handleFieldChange("caspillar_refill_time", ev.target.value)
-                  }
-                  label="CRT"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
-                <TextField
-                  value={treatment.body_condition_score}
-                  onChange={(ev) =>
-                    handleFieldChange("body_condition_score", ev.target.value)
-                  }
-                  label="BCS"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
-                <TextField
-                  value={treatment.fluid_rate}
-                  onChange={(ev) =>
-                    handleFieldChange("fluid_rate", ev.target.value)
-                  }
-                  label="FR"
-                  variant="standard"
-                  size="small"
-                  type="number"
-                  required
-                />
-              </Stack>
-            </Stack>
+          {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          )}
+          {treatment.id ? (
+            <Typography variant="body1">Date: {treatment.date}</Typography>
+          ) : (
+            <Typography variant="body1">
+              Date: {date.toDateString()}{" "}
+            </Typography>
+          )}
+          <Typography variant="body1">Day: {treatment.day} </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              paddingTop: "15px",
+            }}
+          >
             <TextField
-              value={treatment.comments}
-              onChange={(ev) => handleFieldChange("comments", ev.target.value)}
-              label="Comments"
+              sx={{ width: "48%" }}
+              value={treatment.diagnosis}
+              onChange={(ev) => handleFieldChange("diagnosis", ev.target.value)}
+              label="Diagnosis/Findings"
               variant="outlined"
               size="small"
-              type="text"
-              multiline
-              rows={5}
-              fullWidth
               required
+              InputProps={{
+                readOnly: edittreatment ? false : true,
+              }}
             />
-            <br></br>
-            <Button color="primary" variant="contained" type="submit" sx={{marginTop:"15px"}} fullWidth>
-              Save
-            </Button>
-          </form>
-        )}
+            <TextField
+              sx={{ width: "48%" }}
+              value={`${pet.name} (Breed: ${breed.breed})`}
+              onChange={(ev) =>
+                handleFieldChange("body_weight", ev.target.value)
+              }
+              label="Pet"
+              size="small"
+              required
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+          </Box>
+
+          <Stack flexDirection={"row"} justifyContent={"space-evenly"}>
+            <Stack display={"flex"} flexDirection={"column"} padding={"10px"}>
+              <TextField
+                value={treatment.body_weight}
+                onChange={(ev) =>
+                  handleFieldChange("body_weight", ev.target.value)
+                }
+                label="BW"
+                variant="standard"
+                size="small"
+                required
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+              <TextField
+                value={treatment.heart_rate}
+                onChange={(ev) =>
+                  handleFieldChange("heart_rate", ev.target.value)
+                }
+                label="HR"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+              <TextField
+                value={treatment.mucous_membranes}
+                onChange={(ev) =>
+                  handleFieldChange("mucous_membranes", ev.target.value)
+                }
+                label="MM"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+            </Stack>
+            <Stack display={"flex"} flexDirection={"column"} padding={"10px"}>
+              <TextField
+                value={treatment.pr_prealbumin}
+                onChange={(ev) =>
+                  handleFieldChange("pr_prealbumin", ev.target.value)
+                }
+                label="PR"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+              <TextField
+                value={treatment.temperature}
+                onChange={(ev) =>
+                  handleFieldChange("temperature", ev.target.value)
+                }
+                label="Temp"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+              <TextField
+                value={treatment.respiration_rate}
+                onChange={(ev) =>
+                  handleFieldChange("respiration_rate", ev.target.value)
+                }
+                label="RR"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+            </Stack>
+            <Stack display={"flex"} flexDirection={"column"} padding={"10px"}>
+              <TextField
+                value={treatment.caspillar_refill_time}
+                onChange={(ev) =>
+                  handleFieldChange("caspillar_refill_time", ev.target.value)
+                }
+                label="CRT"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+              <TextField
+                value={treatment.body_condition_score}
+                onChange={(ev) =>
+                  handleFieldChange("body_condition_score", ev.target.value)
+                }
+                label="BCS"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+              <TextField
+                value={treatment.fluid_rate}
+                onChange={(ev) =>
+                  handleFieldChange("fluid_rate", ev.target.value)
+                }
+                label="FR"
+                variant="standard"
+                size="small"
+                type="number"
+                InputProps={{
+                  readOnly: edittreatment ? false : true,
+                }}
+              />
+            </Stack>
+          </Stack>
+          <TextField
+            value={treatment.comments}
+            onChange={(ev) => handleFieldChange("comments", ev.target.value)}
+            label="Comments"
+            variant="outlined"
+            size="small"
+            type="text"
+            InputProps={{
+              readOnly: edittreatment ? false : true,
+            }}
+            multiline
+            rows={2}
+            fullWidth
+          />
+          <Box display="flex" justifyContent={"right"}>
+            {edittreatment && (
+              <>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="success"
+                  size="small"
+                  sx={{ mt: 1 }}
+                >
+                  Save
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="error"
+                  size="small"
+                  sx={{ mt: 1, ml: 1 }}
+                  onClick={()=>setEdittreatment(false)}
+                >
+                  cancel
+                </Button>
+              </>
+            )}
+            {!edittreatment && (
+              <Button
+                onClick={onEdit}
+                variant="contained"
+                color="info"
+                size="small"
+                sx={{ mt: 1 }}
+              >
+                Edit
+              </Button>
+            )}{" "}
+          </Box>
+        </form>
       </Stack>
-      <Stack sx={{ display: "flex", flexDirection: "column" }}>
-        <PetConditionAdmission tid={treatment.id} />
-        <PetMedicationAdmission tid={treatment.id} />
-      </Stack>
+      <Divider/>
+      <PetConditionAdmission tid={treatment.id} />
+      <Divider/>
+      <PetMedicationAdmission tid={treatment.id} />
     </Paper>
   );
 }

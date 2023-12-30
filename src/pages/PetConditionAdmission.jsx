771f4e8c14ff90f92e433dae716ec6ed19ea223c
Dianne Bastillada <diannebastillada87@gmail.com>
2023-12-30 18:05:15 +0800

    pet condition modified

--- src/pages/PetConditionAdmission.jsx
+++ src/pages/PetConditionAdmission.jsx
@@ -3,43 +3,22 @@ import axiosClient from "../axios-client";
 import {
   Alert,
   Box,
-  Button,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
   IconButton,
-  Paper,
-  Radio,
-  RadioGroup,
   Stack,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
-  TablePagination,
   TableRow,
   TextField,
   Typography,
 } from "@mui/material";
-import {
-  Add,
-  Archive,
-  Check,
-  Close,
-  Delete,
-  Edit,
-  Refresh,
-  Save,
-} from "@mui/icons-material";
-import { ContextProvider, useStateContext } from "../contexts/ContextProvider";
+import { Add, Archive, Close, Edit, Refresh, Save } from "@mui/icons-material";
 
 export default function PetConditionAdmission({ tid }) {
-
-  const {notification, setNotification} = useStateContext();
   //for table
   const columns = [
-    // { id: "id", name: "ID" },
     { id: "AM/PM", name: "AM/PM" },
     { id: "eating", name: "eating" },
     { id: "drinking", name: "drinking" },
@@ -51,9 +30,10 @@ export default function PetConditionAdmission({ tid }) {
 
   const [loading, setLoading] = useState(false);
   const [petconditions, setPetconditions] = useState([]);
-  // const [notification, setNotification] = useState("");
+  const [message, setMessage] = useState("");
 
   const getTreatmentPetCondition = () => {
+    setMessage("");
     setLoading(true);
     axiosClient
       .get(`/treatments/${tid}/petconditions`)
@@ -61,7 +41,11 @@ export default function PetConditionAdmission({ tid }) {
         setLoading(false);
         setPetconditions(data.data);
       })
-      .catch(() => {
+      .catch((mes) => {
+        const response = mes.response;
+        if (response && response.status == 404) {
+          setMessage(response.data.message);
+        }
         setLoading(false);
       });
   };
@@ -71,6 +55,7 @@ export default function PetConditionAdmission({ tid }) {
 
   const handlePetCondition = () => {
     setAddbtn(true);
+    setPetcondition({});
   };
 
   const handleClose = () => {
@@ -79,7 +64,6 @@ export default function PetConditionAdmission({ tid }) {
 
   //for modal
   const [errors, setErrors] = useState(null);
-  const [modalloading, setModalloading] = useState(false);
   const [petcondition, setPetcondition] = useState({
     id: null,
     is_AM_or_PM: "",
@@ -89,31 +73,16 @@ export default function PetConditionAdmission({ tid }) {
     vomit: "",
     defecated: "",
   });
-  const [open, openchange] = useState(false);
-
-  const addModal = (ev) => {
-    setErrors(null);
-    openchange(true);
-  };
-
-  const closepopup = () => {
-    openchange(false);
-  };
 
   const onEdit = (r) => {
     setErrors(null);
-    // setModalloading(true);
-    setAddbtn(true)
+    setAddbtn(true);
     axiosClient
       .get(`/petconditions/${r.id}`)
       .then(({ data }) => {
-        // setModalloading(false);
         setPetcondition(data);
       })
-      .catch(() => {
-        // setModalloading(false);
-      });
-    openchange(true);
+      .catch(() => {});
   };
 
   const onArchive = (u) => {
@@ -122,7 +91,6 @@ export default function PetConditionAdmission({ tid }) {
     }
 
     axiosClient.delete(`/users/${u.id}/archive`).then(() => {
-      setNotification("User was archived");
       getTreatmentPetCondition();
     });
   };
@@ -134,10 +102,8 @@ export default function PetConditionAdmission({ tid }) {
       axiosClient
         .put(`/petconditions/${petcondition.id}`, petcondition)
         .then(() => {
-          setNotification("Pet condition was successfully updated.");
-          openchange(false);
           getTreatmentPetCondition();
-          setAddbtn(false)
+          setAddbtn(false);
         })
         .catch((err) => {
           const response = err.response;
@@ -149,10 +115,8 @@ export default function PetConditionAdmission({ tid }) {
       axiosClient
         .post(`/petconditions/treatment/${tid}`, petcondition)
         .then(() => {
-          setNotification("Pet condition was successfully saved.");
-          openchange(false);
           getTreatmentPetCondition();
-          setAddbtn(false)
+          setAddbtn(false);
         })
         .catch((err) => {
           const response = err.response;
@@ -173,29 +137,39 @@ export default function PetConditionAdmission({ tid }) {
     getTreatmentPetCondition();
   };
 
-  useEffect(() => {
-    if(tid){
-    getTreatmentPetCondition();
-    }
-  }, []);
-
   return (
     <>
-        {notification && <Alert severity="success">{notification}</Alert>}
-      <Stack sx={{ margin:"10px", border: "1px solid black" }}>
-        <Box sx={{display:"flex", justifyContent:"space-between",  margin:"5px" }}>
-          <Typography> <IconButton color="success" onClick={handleRefresh}>
-            <Refresh />
-          </IconButton>Pet Condition:</Typography>
-        <IconButton
-          color="success"
-          variant="contained"
-          onClick={handlePetCondition}
+      <Stack sx={{ margin: "5px" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
         >
-          <Add/>
-        </IconButton>
+          <Typography>
+            <IconButton color="success" onClick={handleRefresh}>
+              <Refresh />
+            </IconButton>
+            Pet Condition:
+          </Typography>
+          <IconButton
+            color="success"
+            variant="contained"
+            onClick={handlePetCondition}
+          >
+            <Add />
+          </IconButton>
         </Box>
-        <TableContainer sx={{ height: 300,  }} maxwidth="sm">
+        <TableContainer maxwidth="sm">
+          {errors && (
+            <Box>
+              {Object.keys(errors).map((key) => (
+                <Alert severity="error" key={key}>
+                  {errors[key][0]}
+                </Alert>
+              ))}
+            </Box>
+          )}
           <Table stickyHeader aria-label="sticky table">
             <TableHead>
               <TableRow>
@@ -207,96 +181,23 @@ export default function PetConditionAdmission({ tid }) {
             {loading && (
               <TableBody>
                 <TableRow>
-                  <TableCell colSpan={6} style={{ textAlign: "center" }}>
+                  <TableCell
+                    colSpan={columns.length}
+                    style={{ textAlign: "center" }}
+                  >
                     Loading...
                   </TableCell>
                 </TableRow>
               </TableBody>
             )}
-
-            {addbtn && (
+            {!loading && !addbtn && message && (
               <TableBody>
                 <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell>
-                    <TextField
-                      value={petcondition.eating}
-                      onChange={(ev) =>
-                        handleFieldChange("eating", ev.target.value)
-                      }
-                      label="eating"
-                      variant="standard"
-                      size="small"
-                      required
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <TextField
-                      value={petcondition.drinking}
-                      onChange={(ev) =>
-                        handleFieldChange("drinking", ev.target.value)
-                      }
-                      label="drinking"
-                      variant="standard"
-                      size="small"
-                      required
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <TextField
-                      value={petcondition.urinated}
-                      onChange={(ev) =>
-                        handleFieldChange("urinated", ev.target.value)
-                      }
-                      label="urinated"
-                      variant="standard"
-                      size="small"
-                      required
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <TextField
-                      value={petcondition.vomit}
-                      onChange={(ev) =>
-                        handleFieldChange("vomit", ev.target.value)
-                      }
-                      label="vomit"
-                      variant="standard"
-                      size="small"
-                      required
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <TextField
-                      value={petcondition.defecated}
-                      onChange={(ev) =>
-                        handleFieldChange("defecated", ev.target.value)
-                      }
-                      label="defecated"
-                      variant="standard"
-                      size="small"
-                      required
-                    />
-                  </TableCell>
-                  <TableCell>
-                  <Stack direction="row">
-                    <IconButton
-                      variant="contained"
-                      size="small"
-                      color="success"
-                      onClick={(e) => onSubmit(e)}
-                    >
-                      <Save fontSize="small" />
-                    </IconButton>
-                    <IconButton
-                      variant="contained"
-                      size="small"
-                      color="error"
-                      onClick={handleClose}
-                    >
-                      <Close fontSize="small" />
-                    </IconButton>
-                    </Stack>
+                  <TableCell
+                    colSpan={columns.length}
+                    style={{ textAlign: "center" }}
+                  >
+                    {message}
                   </TableCell>
                 </TableRow>
               </TableBody>
@@ -307,7 +208,6 @@ export default function PetConditionAdmission({ tid }) {
                 {petconditions &&
                   petconditions.map((r) => (
                     <TableRow hover role="checkbox" key={r.id}>
-                      {/* <TableCell>{r.id}</TableCell> */}
                       <TableCell>{r.is_AM_or_PM}</TableCell>
                       <TableCell>{r.eating}</TableCell>
                       <TableCell>{r.drinking}</TableCell>
@@ -336,6 +236,88 @@ export default function PetConditionAdmission({ tid }) {
                       </TableCell>
                     </TableRow>
                   ))}
+              </TableBody>
+            )}
+            {addbtn && (
+              <TableBody>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <TextField
+                      value={petcondition.eating || ""}
+                      onChange={(ev) =>
+                        handleFieldChange("eating", ev.target.value)
+                      }
+                      label="eating"
+                      variant="standard"
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      value={petcondition.drinking || ""}
+                      onChange={(ev) =>
+                        handleFieldChange("drinking", ev.target.value)
+                      }
+                      label="drinking"
+                      variant="standard"
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      value={petcondition.urinated || ""}
+                      onChange={(ev) =>
+                        handleFieldChange("urinated", ev.target.value)
+                      }
+                      label="urinated"
+                      variant="standard"
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      value={petcondition.vomit || ""}
+                      onChange={(ev) =>
+                        handleFieldChange("vomit", ev.target.value)
+                      }
+                      label="vomit"
+                      variant="standard"
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      value={petcondition.defecated || ""}
+                      onChange={(ev) =>
+                        handleFieldChange("defecated", ev.target.value)
+                      }
+                      label="defecated"
+                      variant="standard"
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Stack direction="row">
+                      <IconButton
+                        variant="contained"
+                        size="small"
+                        color="success"
+                        onClick={(e) => onSubmit(e)}
+                      >
+                        <Save fontSize="small" />
+                      </IconButton>
+                      <IconButton
+                        variant="contained"
+                        size="small"
+                        color="error"
+                        onClick={handleClose}
+                      >
+                        <Close fontSize="small" />
+                      </IconButton>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
               </TableBody>
             )}
           </Table>

94488bce39c82369ae1e0a76b95bbe636ca73707
Dianne Bastillada <diannebastillada87@gmail.com>
2024-01-14 09:22:31 +0800

    petowner and staff modal zipcode modified

--- src/components/modals/PetOwnerEdit.jsx
+++ src/components/modals/PetOwnerEdit.jsx
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Alert,
-  Autocomplete,
-  Backdrop,
   Box,
   Button,
-  CircularProgress,
   Dialog,
   DialogContent,
   DialogTitle,
@@ -23,41 +20,32 @@ export default function PetOwnerEdit(props) {
     open,
     onClose,
     onClick,
-    id,
     onSubmit,
     loading,
     petowner,
     setPetowner,
     address,
     setAddress,
-    zipcode,
     errors,
     isUpdate,
-    // value,
-    // setValue,
-    zipcodeid,
-    value,
-    setValue,
-    // options,
+    zipcode,
+    selectedZipcode,
+    handleZipcodeChange,
+    zipcodeerror,
   } = props;
 
   const handleFieldChange = (fieldName, value) => {
-    // Create a copy of the user object and update the specified field
     const updatedPetOwner = { ...petowner, [fieldName]: value };
-    // Update the user object with the updated value
     setPetowner(updatedPetOwner);
   };
 
   const handleFieldChangeAddress = (fieldName, value) => {
-    // Create a copy of the user object and update the specified field
     const updatedAddress = { ...address, [fieldName]: value };
-    // Update the user object with the updated value
     setAddress(updatedAddress);
   };
 
   return (
     <>
-     
       {!loading && (
         <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
           <DialogTitle>
@@ -76,129 +64,119 @@ export default function PetOwnerEdit(props) {
                 ))}
               </Box>
             )}
-            <form onSubmit={(e) => onSubmit(e)} on >
-            <Stack spacing={2} margin={2}>
-              <TextField
-                variant="outlined"
-                id="firstname"
-                label="Firstname"
-                value={petowner.firstname || ""}
-                onChange={(ev) =>
-                  handleFieldChange("firstname", ev.target.value)
-                }
-                required
-              />
-              <TextField
-                variant="outlined"
-                id="Lastname"
-                label="Lastname"
-                value={petowner.lastname || ""}
-                onChange={(ev) =>
-                  handleFieldChange("lastname", ev.target.value)
-                }
-                required
-              />
-              <TextField
-                variant="outlined"
-                id="Contact Number"
-                label="Contact Number"
-                type="number"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">+63</InputAdornment>
-                  ),
-                }}
-                value={petowner.contact_num || ""}
-                onChange={(ev) => {
-                  const input = ev.target.value.replace(/\D/g, "").slice(0, 10);
-                  handleFieldChange("contact_num", input);
-                }}
-                required
-              />
+            <form onSubmit={(e) => onSubmit(e)}>
+              <Stack spacing={2} margin={2}>
+                <TextField
+                  variant="outlined"
+                  id="firstname"
+                  label="Firstname"
+                  value={petowner.firstname || ""}
+                  onChange={(ev) =>
+                    handleFieldChange("firstname", ev.target.value)
+                  }
+                  required
+                />
+                <TextField
+                  variant="outlined"
+                  id="Lastname"
+                  label="Lastname"
+                  value={petowner.lastname || ""}
+                  onChange={(ev) =>
+                    handleFieldChange("lastname", ev.target.value)
+                  }
+                  required
+                />
+                <TextField
+                  variant="outlined"
+                  id="Contact Number"
+                  label="Contact Number"
+                  type="number"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">+63</InputAdornment>
+                    ),
+                  }}
+                  value={petowner.contact_num || ""}
+                  onChange={(ev) => {
+                    const input = ev.target.value
+                      .replace(/\D/g, "")
+                      .slice(0, 10);
+                    handleFieldChange("contact_num", input);
+                  }}
+                  required
+                />
 
-              <TextField
-                variant="outlined"
-                id="Zone"
-                label="Zone"
-                value={address.zone}
-                onChange={(ev) =>
-                  handleFieldChangeAddress("zone", ev.target.value)
-                }
-                required
-              />
-              <TextField
-                variant="outlined"
-                id="Barangay"
-                label="Barangay"
-                value={address.barangay}
-                onChange={(ev) =>
-                  handleFieldChangeAddress("barangay", ev.target.value)
-                }
-                required
-              />
-              {/* <Autocomplete
-                sx={{ width: "100%" }}
-                getOptionLabel={(option) =>
-                  `${option.area}, ${option.province}, ${option.zipcode}`
-                }
-                options={zipcode}
-                isOptionEqualToValue={(option, value) => option.id === value.id}
-                noOptionsText="Not Found"
-                renderOption={(props, option) => (
-                  <li {...props}>
-                    {option.area}, {option.province}, {option.zipcode}
-                  </li>
-                )}
-                renderInput={(params) => (
-                  <TextField {...params} label="City, Province, Zipcode" />
-                )}
-                // value={address.zipcode_id || null}
+                <TextField
+                  variant="outlined"
+                  id="Zone"
+                  label="Zone"
+                  value={address.zone}
+                  onChange={(ev) =>
+                    handleFieldChangeAddress("zone", ev.target.value)
+                  }
+                  required
+                />
+                <TextField
+                  variant="outlined"
+                  id="Barangay"
+                  label="Barangay"
+                  value={address.barangay}
+                  onChange={(ev) =>
+                    handleFieldChangeAddress("barangay", ev.target.value)
+                  }
+                  required
+                />
 
-                // key={address.zipcode_id}
-                value={address.zipcode_id || null}
-                onChange={(event, newValue) => {
-                  console.log(newValue);
-                  handleFieldChangeAddress((prevAddress) => ({
-                    ...prevAddress,
-                    zipcode_id: newValue ? newValue.id : null,
-                  }));
-                }}
-              /> */}
+                <Box
+                  display={"flex"}
+                  flexDirection={"row"}
+                  sx={{ width: "100%" }}
+                >
+                  <TextField
+                    id="Zipcode"
+                    label="Zipcode"
+                    type="number"
+                    value={selectedZipcode || ""}
+                    onChange={handleZipcodeChange}
+                    required
+                    error={
+                      (errors && errors.zipcode_id) || zipcodeerror
+                        ? true
+                        : false
+                    }
+                    helperText={(errors && errors.zipcode_id) || zipcodeerror}
+                  />
 
-              
-            <Autocomplete
-              sx={{ width: "100%" }}
-              getOptionLabel={(address) =>
-                `${address.area}, ${address.province}, ${address.zipcode}`
-              }
-              options={zipcode}
-              isOptionEqualToValue={(option, value) =>
-                option.area === value.area
-              }
-              noOptionsText="Not Found"
-              renderOption={(props, address) => (
-                <Box component="li" {...props} key={address.id}>
-                  {address.area}, {address.province}, {address.zipcode}
+                  <Box>
+                    <TextField
+                      id="Area"
+                      label="Area"
+                      value={zipcode.area ? zipcode.area : "..."}
+                      required
+                      InputProps={{
+                        readOnly: true,
+                        "aria-readonly": true,
+                      }}
+                    />
+                  </Box>
+                  <Box>
+                    <TextField
+                      id="Province"
+                      label="Province"
+                      value={zipcode.province ? zipcode.province : "..."}
+                      fullWidth
+                      required
+                      InputProps={{
+                        readOnly: true,
+                        "aria-readonly": true,
+                      }}
+                    />
+                  </Box>
                 </Box>
-              )}
-              renderInput={(params) => (
-                <TextField {...params} label="City, Province, Zipcode" />
-              )}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-                setAddress({
-                  ...address,
-                  zipcode_id: newValue ? newValue.id : "",
-                });
-              }}
-              value={value}
-              required
-            />
-
-              <Button color="primary" variant="contained" type="submit">
-                Save
-              </Button>
-            </Stack>
+                <Button color="primary" variant="contained" type="submit">
+                  Save
+                </Button>
+              </Stack>
             </form>
           </DialogContent>
         </Dialog>
